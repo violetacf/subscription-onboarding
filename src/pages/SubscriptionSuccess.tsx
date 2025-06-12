b7e2087ed19402f37d1e16ca2ccc1e08c1588416
violetacf <violeta@cique.es>
2025-06-12 22:16:44 +0200

    Added animation to subscription success and style module

--- src/pages/SubscriptionSuccess.tsx
+++ src/pages/SubscriptionSuccess.tsx
@@ -1,21 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import styles from "../styles/pages/SubscriptionSuccess.module.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function SubscriptionSuccess() {
   const navigate = useNavigate();
 
   return (
     <div>
-      <h2>Congrats! You're now a subscriber!</h2>
+      <h2>
+        Congrats! You're now a{" "}
+        <span className={styles.subscriberHighlight}>subscriber</span>!
+      </h2>
       <p>Explore your membership now.</p>
-      <hr />
-      <Button
-        onClick={() => navigate("/tech-test-notes")}
-        variant="primary"
-        color="#8a2be2"
-      >
+      <DotLottieReact
+        src="https://lottie.host/bf15bc66-062d-4245-af75-ca96dec1ec56/O8CbcuDf0C.lottie"
+        loop
+        autoplay
+      />
+      <Button onClick={() => navigate("/tech-test-notes")} variant="primary">
         View Tech Test Notes
       </Button>
+      <DotLottieReact
+        src="https://lottie.host/bf15bc66-062d-4245-af75-ca96dec1ec56/O8CbcuDf0C.lottie"
+        loop
+        autoplay
+      />
     </div>
   );
 }
